809e1bb990c617cb2be14e3221ed0faf019c3243
Francisco <franciscocahe@gmail.com>
2022-09-12 17:37:27 -0300

    commit

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -14,29 +14,29 @@ INSERT INTO PERSONAS (NOMBRE, APELLIDO, NUMERO_DE_DOCUMENTO,TIPO_DE_DOCUMENTO,SE
 			VALUES('Bautista','Rodriguez','42123123','DNI','X','ARGENTINA','2000-09-30','brodriguez@gmail.com','1123232356', 4,current_timestamp,current_timestamp)
 ON CONFLICT DO NOTHING;
 INSERT INTO PERSONAS (NOMBRE, APELLIDO, NUMERO_DE_DOCUMENTO,TIPO_DE_DOCUMENTO,SEXO,PAIS,FECHA_DE_NACIMIENTO,EMAIL,TELEFONO,PADRE_PERSONA_ID,CREATED,UPDATED)
-			VALUES('Marcelo','Marquez','2222222','DNI','M','URUGUAY','1975-11-13','bmarquez@gmail.com','3344556699', null,current_timestamp,current_timestamp)
+			VALUES('Marcelo','Marquez','2222222','CI','M','URUGUAY','1975-11-13','bmarquez@gmail.com','3344556699', null,current_timestamp,current_timestamp)
 ON CONFLICT DO NOTHING;
 INSERT INTO PERSONAS (NOMBRE, APELLIDO, NUMERO_DE_DOCUMENTO,TIPO_DE_DOCUMENTO,SEXO,PAIS,FECHA_DE_NACIMIENTO,EMAIL,TELEFONO,PADRE_PERSONA_ID,CREATED,UPDATED)
-			VALUES('Viviana','Morales','222333333','DNI','F','PARAGUAY','1980-12-15','vmorales@gmail.com','212258896', null,current_timestamp,current_timestamp)
+			VALUES('Viviana','Morales','222333333','CIC','F','PARAGUAY','1980-12-15','vmorales@gmail.com','212258896', null,current_timestamp,current_timestamp)
 ON CONFLICT DO NOTHING;
 INSERT INTO PERSONAS (NOMBRE, APELLIDO, NUMERO_DE_DOCUMENTO,TIPO_DE_DOCUMENTO,SEXO,PAIS,FECHA_DE_NACIMIENTO,EMAIL,TELEFONO,PADRE_PERSONA_ID,CREATED,UPDATED)
-			VALUES('Claudia','Chavez','44333333','DNI','F','CHILE','1980-06-20','cchavez@gmail.com','55258896', null,current_timestamp,current_timestamp)
+			VALUES('Claudia','Chavez','44333333','CI','F','CHILE','1980-06-20','cchavez@gmail.com','55258896', null,current_timestamp,current_timestamp)
 ON CONFLICT DO NOTHING;
 INSERT INTO PERSONAS (NOMBRE, APELLIDO, NUMERO_DE_DOCUMENTO,TIPO_DE_DOCUMENTO,SEXO,PAIS,FECHA_DE_NACIMIENTO,EMAIL,TELEFONO,PADRE_PERSONA_ID,CREATED,UPDATED)
-			VALUES('Fernando','Fernandez','44555555','DNI','M','CHILE','1985-08-02','ffernandez@gmail.com','64589699', null,current_timestamp,current_timestamp)
+			VALUES('Fernando','Fernandez','44555555','CI','M','CHILE','1985-08-02','ffernandez@gmail.com','64589699', null,current_timestamp,current_timestamp)
 ON CONFLICT DO NOTHING;
 INSERT INTO PERSONAS (NOMBRE, APELLIDO, NUMERO_DE_DOCUMENTO,TIPO_DE_DOCUMENTO,SEXO,PAIS,FECHA_DE_NACIMIENTO,EMAIL,TELEFONO,PADRE_PERSONA_ID,CREATED,UPDATED)
-			VALUES('Mateo','Gimenez','44553355','DNI','M','CHILE','1983-10-07','mgimenez@gmail.com','454586699', null,current_timestamp,current_timestamp)
+			VALUES('Mateo','Gimenez','44553355','CI','M','CHILE','1983-10-07','mgimenez@gmail.com','454586699', null,current_timestamp,current_timestamp)
 ON CONFLICT DO NOTHING;
 INSERT INTO PERSONAS (NOMBRE, APELLIDO, NUMERO_DE_DOCUMENTO,TIPO_DE_DOCUMENTO,SEXO,PAIS,FECHA_DE_NACIMIENTO,EMAIL,TELEFONO,PADRE_PERSONA_ID,CREATED,UPDATED)
 			VALUES('Carmen','Perez','34444444','DNI','F','ARGENTINA','1989-10-19','cperez@gmail.com','32698324', null,current_timestamp,current_timestamp)
 ON CONFLICT DO NOTHING;
 INSERT INTO PERSONAS (NOMBRE, APELLIDO, NUMERO_DE_DOCUMENTO,TIPO_DE_DOCUMENTO,SEXO,PAIS,FECHA_DE_NACIMIENTO,EMAIL,TELEFONO,PADRE_PERSONA_ID,CREATED,UPDATED)
-			VALUES('Julieta','Paez','45558999','DNI','F','PARAGUAY','1970-09-13','jpaez@gmail.com','68473333', null,current_timestamp,current_timestamp)
+			VALUES('Julieta','Paez','45558999','CIC','F','PARAGUAY','1970-09-13','jpaez@gmail.com','68473333', null,current_timestamp,current_timestamp)
 ON CONFLICT DO NOTHING;
 INSERT INTO PERSONAS (NOMBRE, APELLIDO, NUMERO_DE_DOCUMENTO,TIPO_DE_DOCUMENTO,SEXO,PAIS,FECHA_DE_NACIMIENTO,EMAIL,TELEFONO,PADRE_PERSONA_ID,CREATED,UPDATED)
-			VALUES('Abril','Baez','88888888','DNI','F','CHILE','1999-03-13','abaez@gmail.com','99458889', null,current_timestamp,current_timestamp)
+			VALUES('Abril','Baez','88888888','CI','F','CHILE','1999-03-13','abaez@gmail.com','99458889', null,current_timestamp,current_timestamp)
 ON CONFLICT DO NOTHING;
 INSERT INTO PERSONAS (NOMBRE, APELLIDO, NUMERO_DE_DOCUMENTO,TIPO_DE_DOCUMENTO,SEXO,PAIS,FECHA_DE_NACIMIENTO,EMAIL,TELEFONO,PADRE_PERSONA_ID,CREATED,UPDATED)
-			VALUES('Carmen','Juarez','34444444','DNI','M','BOLIVIA','1989-01-23','cjuarez@gmail.com','55468788', null,current_timestamp,current_timestamp)
+			VALUES('Carmen','Juarez','34444444','CC','M','COLOMBIA','1989-01-23','cjuarez@gmail.com','55468788', null,current_timestamp,current_timestamp)
 ON CONFLICT DO NOTHING;
